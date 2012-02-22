a3635f902898fec2726f5af016e04e87525b5a3d
Jens Andersen <jens.andersen@gmail.com>
2012-02-22 16:27:34 +0100

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,18 @@
+# Inherit device configuration for tf201.
+$(call inherit-product, device/asus/tf201/full_tf201.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cm_tf201
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := tf201
+PRODUCT_MODEL := Transformer Prime
+PRODUCT_MANUFACTURER := asus
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=EeePad BUILD_ID=IML74K BUILD_FINGERPRINT=asus/WW_epad/EeePad:4.0.3/IML74K/WW_epad-9.4.2.11-20120117:user/release-keys PRIVATE_BUILD_DESC="WW_epad-user 4.0.3 IML74K WW_epad-9.4.2.11-20120117 release-keys"
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := tf201
