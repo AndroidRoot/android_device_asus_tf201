dedc2d88f57b61934465fcb96137778eea0fade4
Alexander Hofbauer <alex@derhofbauer.at>
2012-07-16 14:36:13 +0200

    Remove keyswap and init.cardhu.keyboard.rc
    Change-Id: Iddbf74ef389b5ce320b970e69472f29cddecc7dc

--- device_tf201.mk
+++ device_tf201.mk
@@ -34,10 +34,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ramdisk/init.cardhu.rc:root/init.cardhu.rc\
     $(LOCAL_PATH)/ramdisk/ueventd.cardhu.rc:root/ueventd.cardhu.rc \
     $(LOCAL_PATH)/ramdisk/init.cardhu.usb.rc:root/init.cardhu.usb.rc \
-    $(LOCAL_PATH)/ramdisk/init.cardhu.keyboard.rc:root/init.cardhu.keyboard.rc \
     $(LOCAL_PATH)/ramdisk/init.cardhu.cpu.rc:root/init.cardhu.cpu.rc \
-    $(LOCAL_PATH)/ramdisk/fstab.cardhu:root/fstab.cardhu \
-    $(LOCAL_PATH)/prebuilt/keyswap::root/sbin/keyswap \
+    $(LOCAL_PATH)/ramdisk/fstab.cardhu:root/fstab.cardhu
 
 # Prebuilt configeration files
 PRODUCT_COPY_FILES += \
