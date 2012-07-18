cfb3928f572ecc20330276e19f4d23bb83de0120
Bumble-Bee <bigbeeshane@gmail.com>
2012-07-18 17:27:16 +0100

    audio : rename for TF201

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -1331,7 +1331,7 @@ struct audio_module HAL_MODULE_INFO_SYM = {
         .module_api_version = AUDIO_MODULE_API_VERSION_0_1,
         .hal_api_version = HARDWARE_HAL_API_VERSION,
         .id = AUDIO_HARDWARE_MODULE_ID,
-        .name = "Grouper audio HW HAL",
+        .name = "TF201 audio HW HAL",
         .author = "The Android Open Source Project",
         .methods = &hal_module_methods,
     },
