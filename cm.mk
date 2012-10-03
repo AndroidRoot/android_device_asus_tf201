05b8ce98e26a237a027119374f5e6bee1c9ca09d
Bumble-Bee <bigbeeshane@gmail.com>
2012-10-03 10:21:42 +0100

    Update build overrides

--- cm.mk
+++ cm.mk
@@ -10,9 +10,9 @@ $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 PRODUCT_NAME := cm_tf201
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := tf201
-PRODUCT_MODEL := Transformer Prime TF201
+PRODUCT_MODEL := EPAD
 PRODUCT_MANUFACTURER := asus
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=EeePad BUILD_FINGERPRINT=asus/WW_epad/EeePad:4.0.3/IML74K/WW_epad-9.4.2.28-20120525:user/release-keys PRIVATE_BUILD_DESC="WW_epad-user 4.0.3 IML74K WW_epad-9.4.2.28-20120525 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=EeePad BUILD_FINGERPRINT=asus/US_epad/EeePad:4.1.1/JRO03C/US_epad-10.4.2.15-20120917:user/release-keys PRIVATE_BUILD_DESC="US_epad-user 4.1.1 JRO03C US_epad-10.4.2.15-20120917 release-keys"
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := tf201
