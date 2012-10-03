eb5c404c506f3d2b7bd8f35423467173b13f71a6
Bumble-Bee <bigbeeshane@gmail.com>
2012-10-03 10:18:15 +0100

    fixup /sdcard mount in recovery

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -2,7 +2,7 @@
 VENDOR=asus
 DEVICE=tf201
 OUTDIR=vendor/$VENDOR/$DEVICE
-MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
+MAKEFILE=../$OUTDIR/$DEVICE-vendor-blobs.mk
 
 (cat << EOF) > $MAKEFILE
 # Copyright (C) 2011 The CyanogenMod Project
@@ -41,7 +41,7 @@ for FILE in `cat proprietary-files.txt`; do
     echo "    $OUTDIR/proprietary/$FILE:system/$FILE$LINEEND" >> $MAKEFILE
 done
 
-(cat << EOF) > ../../../$OUTDIR/$DEVICE-vendor.mk
+(cat << EOF) > ../$OUTDIR/$DEVICE-vendor.mk
 # Copyright (C) 2011 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -64,7 +64,7 @@ DEVICE_PACKAGE_OVERLAYS := vendor/$VENDOR/$DEVICE/overlay
 \$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
 EOF
 
-(cat << EOF) > ../../../$OUTDIR/BoardConfigVendor.mk
+(cat << EOF) > ../$OUTDIR/BoardConfigVendor.mk
 # Copyright (C) 2011 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
